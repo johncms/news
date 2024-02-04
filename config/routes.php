619e37dee8128072bbd9a951fd76c94889ecbc5e
simba77 <simba7@inbox.ru>
2024-02-04 20:06:34 +0300

    Removed optional last slashes because now it handled by router

--- config/routes.php
+++ config/routes.php
@@ -20,13 +20,13 @@ return function (RouteCollection $router) {
     // $router->addPatternMatcher('sectionPath', '[\w/+-]+');
 
     $router->group('/news', function (RouteCollection $route) {
-        $route->get('/search[/]', [SearchController::class, 'index'])->setName('news.search');
-        $route->get('/search_tags[/]', [SearchController::class, 'byTags'])->setName('news.searchByTags');
+        $route->get('/search/', [SearchController::class, 'index'])->setName('news.search');
+        $route->get('/search_tags/', [SearchController::class, 'byTags'])->setName('news.searchByTags');
         $route->post('/add_vote/{article_id:number}/{type_vote:number}/', [VoteController::class, 'add'])->setName('news.addVote');
         $route->get('/comments/{article_id:number}/', [CommentsController::class, 'index'])->setName('news.comments');
         $route->post('/comments/add/{article_id:number}/', [CommentsController::class, 'add'])->setName('news.comments.add');
         $route->post('/comments/del/', [CommentsController::class, 'del'])->setName('news.comments.del');
-        $route->post('/comments/upload_file[/]', [CommentsController::class, 'loadFile'])
+        $route->post('/comments/upload_file/', [CommentsController::class, 'loadFile'])
             ->setName('news.uploadFile')
             ->addMiddleware(AuthorizedUserMiddleware::class);
 
@@ -37,27 +37,28 @@ return function (RouteCollection $router) {
 
     $router->group('/admin/news', function (RouteCollection $route) {
         $route->get('/', [AdminController::class, 'index'])->setName('news.admin.index');
-        $route->get('/content/[{section_id:number}[/]]', [AdminController::class, 'section'])->setName('news.admin.section');
-        $route->get('/settings[/]', [AdminController::class, 'settings'])->setName('news.admin.settings');
-        $route->post('/settings[/]', [AdminController::class, 'settings'])->setName('news.admin.settingsStore');
+        // TODO: Change optional
+        $route->get('/content/[{section_id:number}/]', [AdminController::class, 'section'])->setName('news.admin.section');
+        $route->get('/settings/', [AdminController::class, 'settings'])->setName('news.admin.settings');
+        $route->post('/settings/', [AdminController::class, 'settings'])->setName('news.admin.settingsStore');
 
         // Articles
-        $route->get('/edit_article/{article_id:number}[/]', [AdminArticleController::class, 'edit'])->setName('news.admin.article.edit');
-        $route->post('/edit_article/{article_id:number}[/]', [AdminArticleController::class, 'edit'])->setName('news.admin.article.editStore');
-        $route->get('/add_article/[{section_id:number}[/]]', [AdminArticleController::class, 'add'])->setName('news.admin.article.add');
-        $route->post('/add_article/[{section_id:number}[/]]', [AdminArticleController::class, 'add'])->setName('news.admin.article.addStore');
-        $route->get('/del_article/{article_id:number}[/]', [AdminArticleController::class, 'del'])->setName('news.admin.article.del');
-        $route->post('/del_article/{article_id:number}[/]', [AdminArticleController::class, 'del'])->setName('news.admin.article.delStore');
+        $route->get('/edit_article/{article_id:number}/', [AdminArticleController::class, 'edit'])->setName('news.admin.article.edit');
+        $route->post('/edit_article/{article_id:number}/', [AdminArticleController::class, 'edit'])->setName('news.admin.article.editStore');
+        $route->get('/add_article/[{section_id:number}/]', [AdminArticleController::class, 'add'])->setName('news.admin.article.add');
+        $route->post('/add_article/[{section_id:number}/]', [AdminArticleController::class, 'add'])->setName('news.admin.article.addStore');
+        $route->get('/del_article/{article_id:number}/', [AdminArticleController::class, 'del'])->setName('news.admin.article.del');
+        $route->post('/del_article/{article_id:number}/', [AdminArticleController::class, 'del'])->setName('news.admin.article.delStore');
 
         // Sections
-        $route->get('/add_section/[{section_id:number}[/]]', [AdminSectionController::class, 'add'])->setName('news.admin.sections.add');
-        $route->post('/add_section/[{section_id:number}[/]]', [AdminSectionController::class, 'add'])->setName('news.admin.sections.add_store');
-        $route->get('/edit_section/{section_id:number}[/]', [AdminSectionController::class, 'edit'])->setName('news.admin.sections.edit');
-        $route->post('/edit_section/{section_id:number}[/]', [AdminSectionController::class, 'edit'])->setName('news.admin.sections.edit_store');
-        $route->get('/del_section/{section_id:number}[/]', [AdminSectionController::class, 'del'])->setName('news.admin.sections.del');
-        $route->post('/del_section/{section_id:number}[/]', [AdminSectionController::class, 'del'])->setName('news.admin.sections.del_store');
+        $route->get('/add_section/[{section_id:number}/]', [AdminSectionController::class, 'add'])->setName('news.admin.sections.add');
+        $route->post('/add_section/[{section_id:number}/]', [AdminSectionController::class, 'add'])->setName('news.admin.sections.add_store');
+        $route->get('/edit_section/{section_id:number}/', [AdminSectionController::class, 'edit'])->setName('news.admin.sections.edit');
+        $route->post('/edit_section/{section_id:number}/', [AdminSectionController::class, 'edit'])->setName('news.admin.sections.edit_store');
+        $route->get('/del_section/{section_id:number}/', [AdminSectionController::class, 'del'])->setName('news.admin.sections.del');
+        $route->post('/del_section/{section_id:number}/', [AdminSectionController::class, 'del'])->setName('news.admin.sections.del_store');
 
         // File uploader
-        $route->post('/upload_file[/]', [AdminArticleController::class, 'loadFile'])->setName('news.admin.sections.loadFile');
+        $route->post('/upload_file/', [AdminArticleController::class, 'loadFile'])->setName('news.admin.sections.loadFile');
     })->addMiddleware(new HasRoleMiddleware('admin'));
 };
